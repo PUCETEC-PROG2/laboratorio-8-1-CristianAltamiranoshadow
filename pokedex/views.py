59dd8a855f873ca7df09a4e9dcd517c59fd1dd93
cris <cristianaltamiranoshadow@gmail.com>
2024-07-25 18:38:42 -0500

    trabajo de login

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,17 +1,39 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import loader
-from.models import Pokemon
+from pokedex.forms import PokemonForm
+from .models import Pokemon
+
+#importaciones de librerias de autentificaciónde django
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    pokemons = Pokemon.objects.order_by('type')
-    template = loader.get_template('index.html') 
+    #pokemons = Pokemon.objects.all() #SELECT * FROM pokedex_pokemon
+    pokemons = Pokemon.objects.order_by('type') #SELECT * FROM pokedex_pokemon ORDER BY type
+    template = loader.get_template('index.html')
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
 def pokemon(request, pokemon_id):
-    #select * from pokedex_pokemon where id ='pokemon_id'
-    pokemon= Pokemon.objects.get(id=pokemon_id)
+    #SELECT * FROM pokedex_pokemon WHERE id = 'pokemon_id'
+    pokemon = Pokemon.objects.get(id=pokemon_id)
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
     }
     return HttpResponse(template.render(context, request))
+@login_required
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+        
+    return render(request, 'add_pokemon.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name ="login.html"
+    
